f15b2dae35ef4f5c0aae664cd02e04a7f46c6845
Adam Lukacovic <adam@adamluck.dev>
2024-09-15 18:33:46 +0200

    nvim: migrate to LazyVim

--- nvim/.config/nvim/after/plugin/lualine.lua
+++
@@ -1,9 +0,0 @@
-vim.opt.showmode = false
-require('lualine').setup({
-  options = {
-    icons_enabled = false,
-    theme = 'auto',
-    component_separators = '|',
-    section_separators = '',
-  },
-})
--- nvim/.config/nvim/after/plugin/telescope.lua
+++
@@ -1,8 +0,0 @@
-require('telescope').setup{
-    defaults = {
-        file_ignore_patterns = {"node_modules"}
-    }
-}
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>bf', builtin.buffers, {})
